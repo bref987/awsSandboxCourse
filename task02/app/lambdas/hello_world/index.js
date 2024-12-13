50fd59fd69f99c88c692621a77d0bd137e225fae
Aleh Slutski <st.alehs@gmail.com>
2024-12-13 16:01:34 +0200

    Update lambda function with method and path

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -1,18 +1,19 @@
 exports.handler = async (event) => {
     // Check if the HTTP method is GET and the path is /hello
-    // if (event.httpMethod === 'GET' && event.path === '/hello') {
-    //     const response = {
-    //         statusCode: 200,
-    //         message: 'Hello from Lambda',
-    //     };
-    //     return response;
-    // } else {
-    //     // Return a 400 response for any other requests
-    //     const response = {
-    //         statusCode: 400,
-    //         message: `Bad request syntax or unsupported method. Request path: ${event.path}. HTTP method: ${event.httpMethod}`,
-    //     };
-    //     return response;
-    // }
-    return event;
+    const method = event.requestContext?.http?.method;
+    const path = event.rawPath;
+    if (method === 'GET' && path === '/hello') {
+        const response = {
+            statusCode: 200,
+            message: 'Hello from Lambda',
+        };
+        return response;
+    } else {
+        // Return a 400 response for any other requests
+        const response = {
+            statusCode: 400,
+            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${httpMethod}`,
+        };
+        return response;
+    }
 };
