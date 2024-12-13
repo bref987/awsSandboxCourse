366c7cde5c108b902da8622fb41cd996b59f9328
Aleh Slutski <st.alehs@gmail.com>
2024-12-13 16:45:11 +0200

    Debug lambda

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -2,18 +2,22 @@ exports.handler = async (event) => {
     // Check if the HTTP method is GET and the path is /hello
     const method = event.requestContext?.http?.method;
     const path = event.rawPath;
+    let response;
     if (method === 'GET' && path === '/hello') {
-        const response = {
+        response = {
             statusCode: 200,
             message: 'Hello from Lambda',
         };
-        return response;
     } else {
         // Return a 400 response for any other requests
-        const response = {
+        response = {
             statusCode: 400,
             message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
         };
-        return response;
     }
+    console.log('++++++++++++++++');
+    console.log(method);
+    console.log(path);
+    console.log(response);
+    return response;
 };
