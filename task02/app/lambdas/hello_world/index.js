9aa56942f8aaa0cdea4c00c1aff205d048f516e3
Aleh Slutski <st.alehs@gmail.com>
2024-12-16 10:28:14 +0200

    Log event

--- task02/app/lambdas/hello_world/index.js
+++ task02/app/lambdas/hello_world/index.js
@@ -3,21 +3,26 @@ exports.handler = async (event) => {
     const method = event.requestContext?.http?.method;
     const path = event.rawPath;
     let response;
-    if (method === 'GET' && path === '/hello') {
-        response = {
-            statusCode: 200,
-            message: 'Hello from Lambda',
-        };
-    } else {
-        // Return a 400 response for any other requests
-        response = {
-            statusCode: 400,
-            message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
-        };
-    }
-    console.log(event, 'event+++');
-    console.log(method, 'method+++');
-    console.log(path, 'path+++');
+
+    response = JSON.stringify({
+        statusCode: 200,
+        message: 'Hello from Lambda stringify'
+    });
+    // if (method === 'GET' && path === '/hello') {
+    //     response = {
+    //         statusCode: 200,
+    //         message: 'Hello from Lambda',
+    //     };
+    // } else {
+    //     // Return a 400 response for any other requests
+    //     response = {
+    //         statusCode: 400,
+    //         message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+    //     };
+    // }
+    // console.log(event, 'event+++');
+    // console.log(method, 'method+++');
+    // console.log(path, 'path+++');
     console.log(response,'response+++');
     return response;
 };
