798573c9fedf8625dcee29dd866e6a03fc2b57c1
Aleh Slutski <st.alehs@gmail.com>
2024-12-20 11:31:25 +0200

    Add task03

---
+++ task03/app/lambdas/hello_world/index.js
@@ -0,0 +1,22 @@
+exports.handler = async (event) => {
+    // Check if the HTTP method is GET and the path is /hello
+    // const method = event.requestContext?.http?.method;
+    // const path = event.rawPath;
+    // let response;
+
+    // if (method === 'GET' && path === '/hello') {
+    //     response = JSON.stringify({
+    //         statusCode: 200,
+    //         message: 'Hello from Lambda',
+    //     });
+    // } else {
+    //     // Return a 400 response for any other requests
+    //     response = JSON.stringify({
+    //         statusCode: 400,
+    //         message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
+    //     });
+    // }
+    // return response;
+    console.log(event);
+    return event;
+};
