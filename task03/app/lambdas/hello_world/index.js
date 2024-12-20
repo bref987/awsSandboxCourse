215f8590292c1fe6021cbef21175934a3bed258f
Aleh Slutski <st.alehs@gmail.com>
2024-12-20 12:45:50 +0200

    Update lambda response

--- task03/app/lambdas/hello_world/index.js
+++ task03/app/lambdas/hello_world/index.js
@@ -1,21 +1,3 @@
 exports.handler = async (event) => {
-    // Check if the HTTP method is GET and the path is /hello
-    // const method = event.requestContext?.http?.method;
-    // const path = event.rawPath;
-    // let response;
-
-    // if (method === 'GET' && path === '/hello') {
-    //     response = JSON.stringify({
-    //         statusCode: 200,
-    //         message: 'Hello from Lambda',
-    //     });
-    // } else {
-    //     // Return a 400 response for any other requests
-    //     response = JSON.stringify({
-    //         statusCode: 400,
-    //         message: `Bad request syntax or unsupported method. Request path: ${path}. HTTP method: ${method}`,
-    //     });
-    // }
-    // return response;
     return {statusCode: 200, message: 'Hello from Lambda'};
 };
