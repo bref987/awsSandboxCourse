8cdf2cfabf82f12962ad70b6e5d6b2b6f683f379
Aleh Slutski <st.alehs@gmail.com>
2024-12-22 17:28:07 +0200

    Update lambda config

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -5,54 +5,52 @@ const { v4: uuidv4 } = require('uuid');
 const dynamodb = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event) => {
-    console.log(event);
-    return event;
-    // try {
-    //     // Parse the request body
-    //     const requestBody = JSON.parse(event.body);
-    //     const { principalId, content } = requestBody;
+    try {
+        // Parse the request body
+        const requestBody = JSON.parse(event.body);
+        const { principalId, content } = requestBody;
 
-    //     console.log(requestBody, '+++++++++++++');
+        console.log(requestBody, '+++++++++++++');
         
-    //     // Generate UUID v4 for id
-    //     const id = uuidv4();
+        // Generate UUID v4 for id
+        const id = uuidv4();
         
-    //     // Get the current time in ISO 8601 format
-    //     const createdAt = new Date().toISOString();
+        // Get the current time in ISO 8601 format
+        const createdAt = new Date().toISOString();
         
-    //     // Prepare the item to be saved to DynamoDB
-    //     const item = {
-    //         id: id,
-    //         principalId: principalId,
-    //         createdAt: createdAt,
-    //         body: content
-    //     };
+        // Prepare the item to be saved to DynamoDB
+        const item = {
+            id: id,
+            principalId: principalId,
+            createdAt: createdAt,
+            body: content
+        };
         
-    //     // Save the item to DynamoDB
-    //     await dynamodb.put({
-    //         TableName: 'Events',
-    //         Item: item
-    //     }).promise();
+        // Save the item to DynamoDB
+        await dynamodb.put({
+            TableName: 'Events',
+            Item: item
+        }).promise();
         
-    //     // Return the created event as a response
-    //     return {
-    //         statusCode: 201,
-    //         body: JSON.stringify({
-    //             statusCode: 201,
-    //             event: item
-    //         }),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     };
-    // } catch (error) {
-    //     console.error('Error saving event to DynamoDB:', error);
-    //     return {
-    //         statusCode: 500,
-    //         body: JSON.stringify({ error: 'Could not save event' }),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     };
-    // }
+        // Return the created event as a response
+        return {
+            statusCode: 201,
+            body: JSON.stringify({
+                statusCode: 201,
+                event: item
+            }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    } catch (error) {
+        console.error('Error saving event to DynamoDB:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ error: 'Could not save event' }),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        };
+    }
 };
