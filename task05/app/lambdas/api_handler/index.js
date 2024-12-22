4e50f90fc60b302758145f697b8973c24a6bd8ef
Aleh Slutski <st.alehs@gmail.com>
2024-12-22 18:11:14 +0200

    Update lambda

--- task05/app/lambdas/api_handler/index.js
+++ task05/app/lambdas/api_handler/index.js
@@ -1,58 +1,44 @@
 const AWS = require('aws-sdk');
 const { v4: uuidv4 } = require('uuid');
 
-// Initialize the DynamoDB DocumentClient
-const dynamodb = new AWS.DynamoDB.DocumentClient();
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+const tableName = process.env.table_name;
 
 exports.handler = async (event) => {
-    console.log(event, '+++++++++++++');
-    return event;
-    // try {
-    //     // Parse the request body
-    //     const requestBody = JSON.parse(event.body);
-    //     const { principalId, content } = requestBody;
+    console.log('event body', event.body);
+    try {
+        const requestBody = JSON.parse(event.body);
+        const {principalId, content} = requestBody;
 
-    //     console.log(requestBody, '+++++++++++++');
-        
-    //     // Generate UUID v4 for id
-    //     const id = uuidv4();
-        
-    //     // Get the current time in ISO 8601 format
-    //     const createdAt = new Date().toISOString();
-        
-    //     // Prepare the item to be saved to DynamoDB
-    //     const item = {
-    //         id: id,
-    //         principalId: principalId,
-    //         createdAt: createdAt,
-    //         body: content
-    //     };
-        
-    //     // Save the item to DynamoDB
-    //     await dynamodb.put({
-    //         TableName: 'Events',
-    //         Item: item
-    //     }).promise();
-        
-    //     // Return the created event as a response
-    //     return {
-    //         statusCode: 201,
-    //         body: JSON.stringify({
-    //             statusCode: 201,
-    //             event: item
-    //         }),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     };
-    // } catch (error) {
-    //     console.error('Error saving event to DynamoDB:', error);
-    //     return {
-    //         statusCode: 500,
-    //         body: JSON.stringify({ error: 'Could not save event' }),
-    //         headers: {
-    //             'Content-Type': 'application/json'
-    //         }
-    //     };
-    // }
+    if (!principalId || !content) {
+        return {
+            statusCode: 400,
+            body: JSON.stringify({ message: 'Invalid request body' }),
+        };
+    }
+
+    const item = {
+        id: uuidv4(),
+        principalId: requestBody.principalId,
+        createdAt: new Date().toISOString(),
+        body: requestBody.content,
+    };
+
+    await dynamoDb.put({
+        TableName: tableName,
+        Item: item,
+    }).promise();
+
+    return {
+        statusCode: 201,
+        body: JSON.stringify({ event: item }),
+    };
+    } catch (error) {
+        console.error('Error saving event:', error);
+        return {
+            statusCode: 500,
+            body: JSON.stringify({ message: 'Internal Server Error' }),
+        };
+    }
 };
+ 
